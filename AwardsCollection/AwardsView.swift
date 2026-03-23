6558cf507cae3272e4006145f1944d5af80458c6
AtesiT <atesit@mail.ru>
2026-03-23 17:04:27 +0300

    FIX: GradientRectangles can be transformed by width, height (without frame problems)

---
+++ AwardsCollection/AwardsView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct AwardsView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                ScrollView {
+                    GradientRectangles()
+                        .frame(width: 200, height: 200)
+                }
+            }
+            .navigationBarTitle("Awards")
+        }
+    }
+}
+
+#Preview {
+    AwardsView()
+}
--- AwardsCollection/GradientRectangles.swift
+++ AwardsCollection/GradientRectangles.swift
@@ -2,27 +2,34 @@ import SwiftUI
 
 struct GradientRectangles: View {
     var body: some View {
-        ZStack {
-            ForEach(0..<3) { iteration in
-                Rectangle()
-                    .fill(
-                        LinearGradient(
-                            gradient: Gradient(colors: [.green, .blue]),
-                            startPoint: UnitPoint(x: 0, y: 1),
-                            endPoint: UnitPoint(x: 1, y: 0)
+        GeometryReader { geometry in
+            let width = geometry.size.width
+            let height = geometry.size.height
+            
+            ZStack {
+                ForEach(0..<3) { iteration in
+                    Rectangle()
+                        .fill(
+                            LinearGradient(
+                                gradient: Gradient(colors: [.green, .blue]),
+                                startPoint: UnitPoint(x: 0, y: 1),
+                                endPoint: UnitPoint(x: 1, y: 0)
+                            )
                         )
-                    )
-                    .rotationEffect(.degrees(Double(iteration * 60)))
+                        .frame(width: width * 0.7, height: height * 0.7)
+                        .rotationEffect(.degrees(Double(iteration * 60)))
+                }
+                Image(systemName: "airplane")
+                    .resizable()
+                    .rotationEffect(.degrees(-90))
+                    .opacity(0.5)
+                    .scaleEffect(0.7)
             }
-            Image(systemName: "airplane")
-                .resizable()
-                .rotationEffect(.degrees(-90))
-                .opacity(0.5)
         }
-        .frame(width: 200, height: 200)
     }
 }
 
 #Preview {
     GradientRectangles()
+        .frame(width: 200, height: 200)
 }
