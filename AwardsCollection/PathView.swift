f511cb4e6e6345725bc67868756e3d02f73dcba4
AtesiT <atesit@mail.ru>
2026-03-24 18:15:30 +0300

    ADD: white stroke and car on a road

--- AwardsCollection/PathView.swift
+++ AwardsCollection/PathView.swift
@@ -20,6 +20,28 @@ struct PathView: View {
                 path.addLine(to: CGPoint(x: middle - nearLine, y: nearLine))
                 
             }
+            .fill(
+                Color(
+                    red: 0.4,
+                    green: 0.4,
+                    blue: 0.4
+                )
+            )
+            
+            Path { path in
+                path.move(to: CGPoint(x: middle, y: nearLine))
+                path.addLine(to: CGPoint(x: middle, y: farLine))
+            }
+            .stroke(
+                Color.white,
+                style: StrokeStyle(lineWidth: 3, dash: [ size / nearLine ])
+            )
+            
+            Image(systemName: "car.fill")
+                .resizable()
+                .foregroundStyle(.white)
+                .scaleEffect(nearLine / 100)
+                .offset(x: -size / 7.25)
         }
     }
 }
