95e2393fa8f74a5e8c45e65ffb98bbd7a435dbc6
AtesiT <atesit@mail.ru>
2026-03-25 16:40:12 +0300

    ADD: MainView (that has Button Show Award \ Hide Award and GradientRectangles in the center of View)

---
+++ AwardsCollection/MainView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct MainView: View {
+    @State private var awardIsShowing = false
+    
+    var body: some View {
+        VStack {
+            Button(action: { awardIsShowing.toggle() }) {
+                Text(awardIsShowing ? "Hide Award" : "Show Award")
+                Spacer()
+                Image(systemName: "chevron.up.square")
+                    .scaleEffect(awardIsShowing ? 2 : 1)
+                    .rotationEffect(.degrees(awardIsShowing ? 0 : 180))
+                    .animation(.default, value: awardIsShowing)
+            }
+            Spacer()
+            
+            GradientRectangles()
+                .frame(width: 250, height: 250)
+            
+            Spacer()
+        }
+        .font(.headline)
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
