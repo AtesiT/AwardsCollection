7805c451b503b419664f0d7fd5b6d75df811e543
AtesiT <atesit@mail.ru>
2026-04-02 18:01:22 +0300

    MADE: AwardsView made with LazyVGrid (without the CustomGridView)

--- AwardsCollection/AwardsView.swift
+++ AwardsCollection/AwardsView.swift
@@ -1,20 +1,40 @@
 import SwiftUI
 
+//struct AwardsView: View {
+//    private let awards = Award.awards.filter{ $0.awarded }
+//    
+//    var body: some View {
+//        NavigationStack {
+//            CustomGridView(items: awards, columns: 2) { award in
+//                VStack {
+//                    award.awardView
+//                    Text(award.title)
+//                }
+//            }
+//            .navigationBarTitle("Your awards: \(awards.count)")
+//        }
+//    }
+//}
+
 struct AwardsView: View {
     private let awards = Award.awards.filter{ $0.awarded }
+    private let columns = [GridItem(.adaptive(minimum: 160, maximum: 200))]
     
     var body: some View {
         NavigationStack {
-            CustomGridView(items: awards, columns: 2) { award in
-                VStack {
-                    award.awardView
-                    Text(award.title)
+            LazyVGrid(columns: columns) {
+                ForEach(awards, id: \.title) { award in
+                    VStack {
+                        award.awardView
+                        Text(award.title)
+                    }
                 }
             }
             .navigationBarTitle("Your awards: \(awards.count)")
         }
     }
 }
+
 
 #Preview {
     AwardsView()
